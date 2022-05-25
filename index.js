b4d92090a19d27c98a211e544858018e27b1fc23
subashdhanush <subashdhanush1618@gmail.com>
2022-05-25 22:30:22 +0530

    update token

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express=require('express')
 const cors=require('cors')
 const mongoose=require('mongoose')
 const router=require('./routes/routes')
+const jwt=require('jsonwebtoken')
+const User=require('./models/userModel')
 
 
 const app=express();
@@ -30,3 +32,46 @@ mongoose.connect(URI).then(()=>{
 
 
 })
+
+app.post('/api/register',async(req,res)=>{
+   console.log(req.body)
+   try{
+   const user=await User.create({
+   // name:req.body.name,
+   email:req.body.email,
+   password:req.body.password, 
+   })
+   res.json({status:'ok'})
+   }
+   catch(err){
+     console.log(err)
+     res.json({status:'error',error:'Duplicate email'})
+   }
+ })
+
+ app.post('/api/login',async(req,res)=>{
+   const user=await User.findOne({
+   email:req.body.email,
+   password:req.body.password, 
+   })
+   if(user){
+ 
+   // const token=jwt.sign({
+   
+   //   email:user.email,
+   // }, 'my_secret_key')    
+   
+   const token=jwt.sign({
+   
+     email:user.email,
+   },process.env.SECRET_KEY) 
+ 
+   
+ 
+ 
+   return res.json({status:'ok',user:token})
+   }
+   else{
+     return res.json({status:'error',user:false})
+   }
+ })
--- models/question.js
+++ models/question.js
@@ -9,7 +9,21 @@ const questionSchema=mongoose.Schema({
    answer:{type:String}
 })
 
+const appleSchema=mongoose.Schema({
+   id   :{type: String, require},
+   title : {type: String, require},
+   price :{type: String, require},
+   desc : {type: String, require},
+   img : {type: String, require},
+  },{
+      timestamps:true,
+  })
 
 const questionModel=mongoose.model("question",questionSchema);
 
+const appleModel=mongoose.model('apples',appleSchema)
+
+
 module.exports=questionModel;
+
+module.exports=appleModel;
